f537f2ae8f03d324e6bd289e08ba447a8feef24e
Serena - <serenaferri1998@outlook.it>
2023-05-22 09:31:07 +0100

    THIRD COMMIT

--- client/src/Art.js
+++ client/src/Art.js
@@ -1,7 +1,7 @@
 
 
 const Art = [{
-    id: 1,
+    id: 0,
     url: "https://d3d00swyhr67nd.cloudfront.net/w800h800/collection/TATE/TATE/TATE_TATE_T05010_10-001.jpg ",
     title: "Pablo Picasso ",
     date: 1937,
@@ -9,7 +9,7 @@ const Art = [{
 
 },
 {
-    id: 2,
+    id: 1,
     url: " https://d7hftxdivxxvm.cloudfront.net/?height=713&quality=80&resize_to=fit&src=https%3A%2F%2Fd32dm0rphc51dk.cloudfront.net%2Faes0eLUVKs0oksAzap8NNw%2Fnormalized.jpg&width=800",
     title: "Takashi Murakami ",
     date: 2016,
@@ -17,15 +17,15 @@ const Art = [{
 
 },
 {
-    id: 3,
+    id: 2,
     url: "https://d7hftxdivxxvm.cloudfront.net/?height=545&quality=80&resize_to=fit&src=https%3A%2F%2Fd32dm0rphc51dk.cloudfront.net%2FxVepiD8MIOhy1OzK7tyJuw%2Fnormalized.jpg&width=800 ",
     title: "Katsushika Hokusai",
     date: 1831,
-    title: " 'Great Wave' -Kanagawa oki nami ura.",
+    description: " 'Great Wave' -Kanagawa oki nami ura.",
 
 },
 {
-    id: 4,
+    id: 3,
     url: "https://m.media-amazon.com/images/I/91aSs9uNGRL._AC_SL1500_.jpg",
     title: "Gustav Klimt ",
     date: "1907 - 1908 ",
@@ -33,14 +33,14 @@ const Art = [{
 
 },
 {
-    id: 5,
+    id: 4,
     url: " https://cdn.mos.cms.futurecdn.net/m4Fubda7zVYakfQQNMbqmf.jpg",
     title: "Damien Hirst ",
     date: "1991",
     description: "The Physical Impossibility of Death in the Mind of Someone Living. ",
 },
 {
-    id: 6,
+    id: 5,
     url: " https://th-thumbnailer.cdn-si-edu.com/fAuWENxGckn5QnvgiKB-YFsXek4=/fit-in/1072x0/https://tf-cmsv2-smithsonianmag-media.s3.amazonaws.com/filer/3b/8a/3b8aafbf-d9b1-4555-90bd-5b23748d27cf/salvador_dali_somni_causat_pel_vol_duna_abella_al_voltant_duna_magrana_un_segon_abans_de_despertar_c_salvador_dali_fundacio_gala-salvador_dali_vegap_barcelona_2018.jpg",
     title: "Salvador Dali'",
     date: 1944,
--- client/src/components/ArtSearch.jsx
+++ client/src/components/ArtSearch.jsx
@@ -2,82 +2,55 @@ import { useState } from "react";
 import App from "../App.css"; 
 
 function ArtSearch({art}) {
-  const [isActive, setIsActive] = useState(false);
+  const [isActive, setIsActive] = useState([false, false, false, false, false, false]);
   
-  function handleShow() {
-     setIsActive(!isActive)
+
+  function handleShow(id) {
+    let newArray = [...isActive]
+   
+    if(newArray[id] === false) {
+      newArray[id] = true;
+    }
+    else if(newArray[id] === true) {
+      newArray[id] = false;
+    } 
+     setIsActive(newArray)
+
+     console.log(isActive);
   }
+
   return (
    <div>
     <h1> Art Search </h1>
         
     {art.map( item => (
      <li key={item.id} >
-      <div className="par">
+      <div className="all-thing">
       <img 
-       className="image-grid"
+       className="img"
        src={item.url}
        alt=""
        />
-       <button className="btn1" onClick={handleShow}>Check description</button>
-        
-       { isActive &&  <div> <p> 
-         <b className="title">{item.title}</b>
+       <button className="btn" onClick = {() => handleShow(item.id)}>Check description</button>
+       <div className="text">
+
+       { isActive[item.id] &&  <div> <p> 
+         <b className="title"style={{fontSize:"xxxLarge", position:"absolute", right: "300px", position:"center", fontFamily:"serif"}} >{item.title}</b>
        </p>
-       <p>
-       <b className="date"> ~{item.date}~</b>
+       <p>   
+       <b className="date" style={{fontSize: "large", position:"relative", right:"-200px", bottom:"-100px"}}> ~{item.date}~</b>
        </p>
-       <p className="description">
-        {item.description}
-       </p> </div> }
-       </div>
+       
+       <p className="description" style={{fontSize:"xLarge", position:"relative",right:"-150px", bottom:"-200px", fontFamily:"cursive"}} > " {item.description} "</p> 
+
+           </div> }
+           </div>
+         </div>
      </li> 
     ))}
      </div>
-  
   );
 } 
 
 
-
-
-
-
-
-
-
-
-
-
 export default ArtSearch; 
-
-
-
-
-
-
-
-
-
-
-
-
-//export default function ArtSearch() {
-  //const [message, setMessage] = useState({
-
-  //});
-
-
-
-
-//function handleInputChange() {
-
-//}
-
-//return (
- // <div>
-   // <h1> </h1>
-    //<input type="text" value={message} onChange={handleInputChange}/>
-  //</div>
-//)
-//}
